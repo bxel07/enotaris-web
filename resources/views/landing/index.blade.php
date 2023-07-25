322e24badd55566b857257ec08c922e24a78f0ea
Zaki <muhammadzaki.513@gmail.com>
2023-07-25 13:42:19 +0700

    update animation

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -49,7 +49,7 @@
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="index.html">Notaris <span class="sp">Digital</span></a></h1>
+      <h1 class="logo me-auto"><a href="/">Notaris <span class="sp">Digital</span></a></h1>
 
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
