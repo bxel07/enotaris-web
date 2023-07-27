a21e57217481a809221e153d4efe5f7b5444f736
bxel <yogipratama.ut@gmail.com>
2023-07-27 14:19:11 +0700

    fix forget styling layout auth

--- resources/views/auth/forgetPasswordLink.blade.php
+++ resources/views/auth/forgetPasswordLink.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.main')
 @section('content')
-    <section class="vh-100 sign-in">
+    <section class="vh-100" style="background:#f48f00 ">
       <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
           <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div class="card shadow-2-strong">
+            <div class="card shadow-2-strong" style="background-color: #ffe3bd; border-radius: 1rem ">
               <div class="card-body p-5 text-center">
                 <h3 class="mb-5">Forget Password</h3>
                 <div class="row g-3">
