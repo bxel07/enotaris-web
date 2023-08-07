ecaa325b92be7f939b9ad9c2532b7a0c4ab5d1bc
Zaki <muhammadzaki.513@gmail.com>
2023-08-07 14:09:52 +0700

    add preview template

---
+++ resources/views/notaris/preview CV.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Document Preview dengan Editable Content</title>
+    <link rel="stylesheet" href="/css/print.css">
+</head>
+
+<body>
+    <div id="documentPreview">
+        <!-- Dokumen untuk pengajuan PT -->
+        <h1>SURAT KUASA</h1>
+        <h4>Yang bertanda tangan di bawah ini :</h4>
+        <p>1. Nama : </p>
+        <p>Tempat & tgl lahir: </p>
+        <p>Pekerjaan: </p>
+        <p>Alamat : </p>
+        <p>Nomor KTP: </p>
+        <br>
+        <p>2. Nama : </p>
+        <p>Tempat & tgl lahir: </p>
+        <p>Pekerjaan: </p>
+        <p>Alamat : </p>
+        <p>Nomor KTP: </p>
+        <br>
+        <p>3. Nama : </p>
+        <p>Tempat & tgl lahir: </p>
+        <p>Pekerjaan: </p>
+        <p>Alamat : </p>
+        <p>Nomor KTP: </p><br>
+
+        <h4>Untuk selanjutnya disebut Pemberi Kuasa, dengan ini memberi kuasa kepada :</h2>
+            <p>Nama : </p>
+            <p>Tempat & tgl lahir : </p>
+            <p>Pekerjaan : </p>
+            <p>Alamat : </p>
+            <p>Nomor KTP : </p>
+            <h4>Untuk selanjutnya disebut Penerima Kuasa</h2>
+            <br><br><br><br><br><br>
+
+            <h2>-------------------------------------------- KHUSUS ----------------------------------------------</h2>
+            <p>untuk dan atas nama Pemberi Kuasa, menghadap dihadapan Notaris untuk menandatangani akta pendirian
+                Perseroan Komanditer CV.________________________________, berkedudukan di ________________, dengan
+                data-data sebagai berikut:</p>
+            <br>
+
+            <p>Nama CV : </p>
+            <p>Tempat Kedudukan : </p>
+            <p>Bidang Usaha (KBLI) : </p>
+            <p>Modal : Rp.</p>
+            <br>
+
+            <h4>Susunan Pengurus : </h4>
+            <p>Persero Aktif : </p>
+            <p>Persero Pasif : </p><br>
+
+            <p>untuk itu Penerima Kuasa berhak menghadap Notaris, memberikan keterangan-keterangan, serta menandatangani
+                akta dan atau dokumen-dokumen/surat-surat yang diperlukan untuk mencapai maksud tersebut di atas.</p>
+            <br>
+            <p>Demikian Surat Kuasa ini dibuat untuk dapat dipergunakan sebagaimana mestinya.</p><br>
+            <div class="signature-container">
+                <div class="signature-box">
+                    <p>Jakarta</p>
+                    <h4>Pemberi Kuasa</h4><br>
+                    <p>Materai 10000</p>
+                    <p>________________________</p>
+                    <!-- Tambahkan elemen lain sesuai kebutuhan, misalnya input tanda tangan elektronik. -->
+                </div>
+
+                <div class="signature-box">
+                    <h4>Penerima Kuasa</h4><br><br><br>
+                    <p>________________________</p>
+                    <!-- Tambahkan elemen lain sesuai kebutuhan, misalnya input tanda tangan elektronik. -->
+                </div>
+            </div>
+    </div>
+
+    <div id="editor">
+        <!-- Editor konten dokumen akan muncul di sini -->
+    </div>
+
+    <script src="https://cdn.ckeditor.com/ckeditor5/30.0.0/classic/ckeditor.js"></script>
+    <script src="print.js"></script>
+</body>
+
+</html>
